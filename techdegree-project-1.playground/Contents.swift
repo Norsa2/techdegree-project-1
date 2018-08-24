8bbf56af037dce0ee0c5eb5e364eb3b9c868d09c
Norsa2 <jn@norsa.no>
2018-08-24 11:57:36 +0200

    Added Players

---
+++ techdegree-project-1.playground/Contents.swift
@@ -0,0 +1,27 @@
+var players: [[String: Any]] = [
+    ["playerName": "Joe Smith", "height": 42.0,"experience": true, "guardians": "Jim and Jan Smith"],
+    ["playerName": "Jill Tanner", "height": 36.0,"experience": true, "guardians": "Clara Tanner"],
+    ["playerName": "Bill Bon", "height": 43.0,"experience": true, "guardians": "Sara and Jenny Bon"],
+    ["playerName": "Eva Gordon", "height": 45.0,"experience": false, "guardians": "Wendy and Mike Gorden"],
+    ["playerName": "Matt Gill", "height": 40.0,"experience": false, "guardians": "Charles and Sylvia Gill"],
+    ["playerName": "Kimmy Stein", "height": 41.0,"experience": false, "guardians": "Bill and Hillary Stein"],
+    ["playerName": "Sammy adams", "height": 45.0,"experience": false, "guardians": "Jeff Adams"],
+    ["playerName": "Karl Saygan", "height": 42.0,"experience": true, "guardians": "Heather Bledsoe"],
+    ["playerName": "Suzane Greenberg", "height": 44.0,"experience": true, "guardians": "Henrietta Dumas"],
+    ["playerName": "Sal Dali", "height": 41.0,"experience": false, "guardians": "Gala Dali"],
+    ["playerName": "Joe Kavalier", "height": 39.0,"experience": false, "guardians": "Sam and Elaine Kavalier"],
+    ["playerName": "Ben Finkelstein", "height": 44.0,"experience": false, "guardians": "Aaron and Jill Finkelstein"],
+    ["playerName": "Diego Soto", "height": 41.0,"experience": true, "guardians": "Robin and Sarika Soto"],
+    ["playerName": "Chloe Alaska", "height": 47.0,"experience": false, "guardians": "David and Jamie Alaska"],
+    ["playerName": "Arnold Willis", "height": 43.0,"experience": false, "guardians": "Claire Willis"],
+    ["playerName": "Phillip Helm", "height": 44.0,"experience": true, "guardians": "Thomas Helm and Eva Jones"],
+    ["playerName": "Les Clay", "height": 42.0,"experience": true, "guardians": "Wynonna Brown"],
+    ["playerName": "Herschel Krustofski", "height": 45.0,"experience": true, "guardians": "Hyman and Rachel Krustofski"]
+]
+var experiencedPlayers: [[String: Any]] = []
+
+var teamSharks: [[String: Any]] = []
+var teamDragons: [[String: Any]] = []
+var teamRaptors: [[String: Any]] = []
+
+var TheLeague: [[String: Any]] = []
